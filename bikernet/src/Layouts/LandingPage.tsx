004a5d5040495280f05235345669a394b5632f94
AlinNast <alin10nastasa@gmail.com>
2022-08-13 10:48:23 -0700

    deleted unnecesary documents

---
+++ bikernet/src/Layouts/LandingPage.tsx
@@ -0,0 +1,12 @@
+
+
+function LandingPage() {
+    return(
+        <>
+        <h1 style={{"color": "red"}}> header </h1>
+        <h1> this is the title</h1>
+        </>
+    )
+}
+
+export default LandingPage;
