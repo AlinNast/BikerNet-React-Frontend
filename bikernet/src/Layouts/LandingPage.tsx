0161fa85149edc9c05ed3b75a25a58bb0a20d06e
AlinNast <alin10nastasa@gmail.com>
2022-08-14 05:13:41 -0700

    designed landing page

--- bikernet/src/Layouts/LandingPage.tsx
+++ bikernet/src/Layouts/LandingPage.tsx
@@ -1,11 +1,16 @@
-
+import './LandingPage.css';
 
 function LandingPage() {
     return(
-        <>
-        <h1 style={{"color": "red"}}> header </h1>
-        <h1> this is the title</h1>
-        </>
+        <div className='main'>
+            <div className="container">
+                <div className='title'>BikerNet</div>
+                <div className="login">Log In</div>
+                <div className="register">Join the brotherhood</div>
+                <div className="end">Grab life by the handlebars</div>
+            </div>
+            
+        </div>
     )
 }
 
